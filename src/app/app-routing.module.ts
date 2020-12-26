b40bd988bfdf98171952f40d1af75a8c4f36d161
Waqil Shopnil <waqilshopnil@gmail.com>
2020-12-26 16:51:06 +0600

    configured authguard for home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,12 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { TemperatureHumidityComponent } from './dashboard/temperature-humidity/temperature-humidity.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
+import { AuthGuard } from './services/authentication-guard.service';
 import { SettingsComponent } from './settings/settings.component';
 
 const routes: Routes = [
   {path:  "", pathMatch:  "full",redirectTo:  "home"},
-  {path: "home", component: HomeComponent},
+  {path: "home", component: HomeComponent, canActivate:[AuthGuard]},
   {path: "dashboard", component: DashboardComponent},
   {path: 'temperature-humidity', component: TemperatureHumidityComponent},
   {path: "profile", component: ProfileComponent},
