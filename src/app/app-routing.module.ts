c93f0a53c11ec78c29f367f2597b9137d4028f87
Rafiul Islam <avoidcloud@gmail.com>
2020-12-26 15:41:01 +0600

    removed unused dashboard component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { TemperatureHumidityComponent } from './dashboard/temperature-humidity/temperature-humidity.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SettingsComponent } from './settings/settings.component';
@@ -9,8 +7,6 @@ import { SettingsComponent } from './settings/settings.component';
 const routes: Routes = [
   {path:  "", pathMatch:  "full",redirectTo:  "home"},
   {path: "home", component: HomeComponent},
-  {path: "dashboard", component: DashboardComponent},
-  {path: 'temperature-humidity', component: TemperatureHumidityComponent},
   {path: "profile", component: ProfileComponent},
   {path: "settings", component: SettingsComponent}
 ];
