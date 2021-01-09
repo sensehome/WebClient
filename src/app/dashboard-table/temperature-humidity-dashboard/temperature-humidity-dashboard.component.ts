484a79964b3847fc0e810ce96d726c7b273f68cd
Waqil Shopnil <waqilshopnil@gmail.com>
2021-01-10 00:18:37 +0600

    segregated temperature and humidity history line chart

--- src/app/dashboard-table/temperature-humidity-dashboard/temperature-humidity-dashboard.component.ts
+++ src/app/dashboard-table/temperature-humidity-dashboard/temperature-humidity-dashboard.component.ts
@@ -24,28 +24,32 @@ export class TemperatureHumidityDashboardComponent implements OnInit, AfterViewI
   //   { data: [65, 59, 80, 81, 56, 55, 40], label: 'My First dataset' },
   //   { data: [28, 48, 40, 19, 86, 27, 90], label: 'My Second dataset' }
   // ];
-  public chartDatasets: Array<any> = [
-    { data: this.humidityValueList, label: 'humidity' },
+  public chartTemperatureDatasets: Array<any> = [
     { data: this.temperatureValueList, label: 'temperature' }
+  ];
+  public chartHumidityDatasets: Array<any> = [
+    { data: this.humidityValueList, label: 'humidity' }
   ];
 
   // public chartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
   public chartLabels: Array<any> = this.dateValueList;
 
 
-  public chartColors: Array<any> = [
+  public chartTemperatureColors: Array<any> = [
     {
       backgroundColor: 'rgba(105, 0, 132, .2)',
       borderColor: 'rgba(200, 99, 132, .7)',
       borderWidth: 2,
-    },
+    }
+  ];
+
+  public chartHumidityColors: Array<any> = [
     {
       backgroundColor: 'rgba(0, 137, 132, .2)',
       borderColor: 'rgba(0, 10, 130, .7)',
       borderWidth: 2,
     }
   ];
-
   public chartOptions: any = {
     responsive: true
   };
